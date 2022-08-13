4cea5578d3b7cf8dcafa2037dc5dc239103022ad
Filip Hobza <filip.hobza.2005@gmail.com>
2022-08-13 23:51:10 +0200

    PlayerMovment update

--- WowieJam/Assets/Scripts/PlayerMovment.cs
+++ WowieJam/Assets/Scripts/PlayerMovment.cs
@@ -8,6 +8,10 @@ public class PlayerMovment : MonoBehaviour
 
     [HideInInspector]public Vector2 playerInput;
     public float speed = 100f;
+    public float jumpForce = 100f;
+    
+    [SerializeField] private Transform firstPoint;
+    [SerializeField] private float raycastDistance;
     
     
     // Update is called once per frame
@@ -22,7 +26,23 @@ public class PlayerMovment : MonoBehaviour
 
         playerInput.x = Input.GetAxisRaw("Horizontal");
 
+        if(Input.GetButtonDown("Jump")&& IsGrounded())
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+        }
 
         rb.AddForce(playerInput*speed*Time.deltaTime);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2D = Physics2D.Raycast(firstPoint.position, Vector2.down, raycastDistance, LayerMask.GetMask("Ground"));
+        if(raycastHit2D.collider != null)
+        {
+            return true;
+        }else
+        {
+            return false;
+        }
     }
 }
