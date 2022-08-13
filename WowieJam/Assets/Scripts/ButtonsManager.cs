0f1ae2d404226bde2b7d1e66a5582d34ab4812b9
Filip Hobza <filip.hobza.2005@gmail.com>
2022-08-13 19:54:15 +0200

    StartScreen update

---
+++ WowieJam/Assets/Scripts/ButtonsManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> menus = new List<GameObject>();
+
+    public void Start()
+    {
+        foreach (var x in menus)
+        {
+            x.SetActive(false);
+        }
+        menus[0].SetActive(true);
+    }
+
+    public void StartGameButton()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    
+    public void CreditsButton()
+    {
+        foreach (var x in menus)
+        {
+            x.SetActive(false);
+        }
+        menus[1].SetActive(true);
+    }
+    
+    public void BackButton()
+    {
+        foreach (var x in menus)
+        {
+            x.SetActive(false);
+        }
+        menus[0].SetActive(true);
+    }
+    
+    public void ExitGameButton()
+    {
+        Application.Quit();
+    }
+}
